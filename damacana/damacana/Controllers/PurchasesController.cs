13012f23213b6fc7ddd1fb1ec67d4dcf7803c074
Jeffrey Beyo <jeffrey_8@hotmail.fr>
2015-12-10 02:54:42 +0200

    purchases try

--- damacana/damacana/Controllers/PurchasesController.cs
+++ damacana/damacana/Controllers/PurchasesController.cs
@@ -14,34 +14,6 @@ namespace damacana.Controllers
 {
     public class PurchasesController : Controller
     {
-        // GET: Purchases
-        public User myuser = new User();
-        public PurchaseDBContext db = new PurchaseDBContext();
-        public static List<Purchase> PurchasesTem = new List<Purchase>
-        {
 
-        };
-
-        public int PurchaseId = 0;
-        public int i = 0;
-        public ActionResult Purchase()
-        {
-
-            Purchase siparis = new Purchase();
-            decimal totalprice = 0;
-            siparis.PurchaseList = new List<Product>();
-            foreach (Product p in (List<Product>)TempData["Cartlist"])
-            {
-                siparis.PurchaseList.Add(p);
-                totalprice = p.Price + totalprice;
-            }
-            siparis.Id = PurchaseId;
-            PurchaseId = PurchaseId + 1;
-            siparis.TotalPrice = totalprice;
-            TempData["Siparis"] = siparis;
-            i++;
-            return View(siparis);
-
-        }
     }
 }
